70d504a2998343b8d055e58587b29aa3c451f93b
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 22:59:16 +0100

    implementacja metody

--- MergeSort.h
+++ MergeSort.h
@@ -7,7 +7,12 @@ template <typename T>
 class MergeSort
 {
 public:
-  
+   /// Sortuje wektor rosnąco
+    void sort(std::vector<T>& arr)
+    {
+        if (arr.size() <= 1) return;
+        mergeSort(arr, 0, arr.size() - 1);
+    }
 private:
   
 #endif
