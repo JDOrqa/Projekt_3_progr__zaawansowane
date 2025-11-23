fb121f88326faf9bb42380f380996c0f347478a4
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 23:07:16 +0100

    Pierwszy test --- Posortowane rosnąco

--- test.cpp
+++ test.cpp
@@ -2,3 +2,10 @@
 #include "gtest/gtest.h"
 #include "../MergeSortProject/MergeSort.h"
 #include <vector>
+
+TEST(MergeSortTests, AlreadySorted) {
+    MergeSort<int> sorter;
+    std::vector<int> arr = { 1, 2, 3, 4, 5 };
+    sorter.sort(arr);
+    EXPECT_EQ(arr, (std::vector<int>{1, 2, 3, 4, 5}));
+}
