7492c58a1abc5394a6b79aab22c8061ebfa58ad2
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 23:04:37 +0100

    Instancja dla double a

--- main.cpp
+++ main.cpp
@@ -18,5 +18,19 @@ int main()
     for (auto x : arrInt) std::cout << x << " ";
     std::cout << std::endl;
 
+     // Instancja dla double
+    MergeSort<double> sorterDouble;
+    std::vector<double> arrDouble = { 5.5, 1.1, -3.3, 9.9, 0.5, -2.7 };
+
+    std::cout << "\nTablica double przed sortowaniem: ";
+    for (auto x : arrDouble) std::cout << x << " ";
+    std::cout << std::endl;
+
+    sorterDouble.sort(arrDouble);
+
+    std::cout << "Tablica double po sortowaniu: ";
+    for (auto x : arrDouble) std::cout << x << " ";
+    std::cout << std::endl;
+
     return 0;
 }
