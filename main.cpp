f90b0f514ba69329849c71ecd6fd6877ec305680
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 23:04:06 +0100

    Instancja dla inta

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@
 
 int main()
 {
-   
+    // Instancja dla int
+    MergeSort<int> sorterInt;
+    std::vector<int> arrInt = { 5, 1, 9, 2, 3, -4, 7, 0 };
+
+    std::cout << "Tablica int przed sortowaniem: ";
+    for (auto x : arrInt) std::cout << x << " ";
+    std::cout << std::endl;
+
+    sorterInt.sort(arrInt);
+
+    std::cout << "Tablica int po sortowaniu: ";
+    for (auto x : arrInt) std::cout << x << " ";
+    std::cout << std::endl;
 
     return 0;
 }
