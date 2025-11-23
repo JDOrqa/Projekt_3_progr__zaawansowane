ccc34087472f2da51ac1707a492ad09c303decf2
Szymon <125341558+JDOrqa@users.noreply.github.com>
2025-11-23 23:00:03 +0100

    implementacja  funkcji mergeSort

--- MergeSort.h
+++ MergeSort.h
@@ -14,5 +14,15 @@ public:
         mergeSort(arr, 0, arr.size() - 1);
     }
 private:
-  
+   void mergeSort(std::vector<T>& arr, int left, int right)
+    {
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
 #endif
